6e9331e12593d5f43cf7e9b7d41a2bcf413ab756
Andreas Duerloo <95074391+andreasduerloo@users.noreply.github.com>
2022-01-14 21:30:04 +0100

    Delete stegosaurus/target directory

--- stegosaurus/target/debug/deps/stegosaurus-f46e4ff871610d47.d
+++
@@ -1,5 +0,0 @@
-D:\Andreas\Programmeren\stegosaurus\stegosaurus\target\debug\deps\stegosaurus-f46e4ff871610d47.rmeta: src\main.rs
-
-D:\Andreas\Programmeren\stegosaurus\stegosaurus\target\debug\deps\stegosaurus-f46e4ff871610d47.d: src\main.rs
-
-src\main.rs:
--- stegosaurus/target/debug/stegosaurus.d
+++
@@ -1,1 +0,0 @@
-D:\Andreas\Programmeren\stegosaurus\stegosaurus\target\debug\stegosaurus.exe: D:\Andreas\Programmeren\stegosaurus\stegosaurus\src\main.rs
